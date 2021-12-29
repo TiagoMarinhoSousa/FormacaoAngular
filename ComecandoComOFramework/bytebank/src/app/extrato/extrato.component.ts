5654310a593dd1750f99262d43e38500f358ea1b
Tiago Marinho Sousa <tiagomarinho9101@gmail.com>
2021-12-29 18:44:52 -0300

    Aula 6 - API Rest, HttpClient, Observable

--- ComecandoComOFramework/bytebank/src/app/extrato/extrato.component.ts
+++ ComecandoComOFramework/bytebank/src/app/extrato/extrato.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Transferencia } from '../models/transferencia.models';
 import { TransferenciaService } from '../services/transferencia.service';
 
 @Component({
@@ -8,11 +10,29 @@ import { TransferenciaService } from '../services/transferencia.service';
 })
 export class ExtratoComponent implements OnInit {
 
-  transferencias: any[];
+  // transferencias: any[];
+
+  //aula 6.4
+
+  transferencias?: Observable<Transferencia[]>
   constructor(private service: TransferenciaService) { }
 
-  ngOnInit() {
-    this.transferencias = this.service.transferencias;
-  }
+  // ngOnInit() {
+  //   this.transferencias = this.service.transferencias;
+  // }
 
+  //consumindo api rest
+
+  // ngOnInit() {
+  //   this.service.todas().subscribe((transferencias: Transferencia[]) => {
+  //     console.table(transferencias);
+  //     this.transferencias = transferencias;
+  //   });
+
+
+  //aula 6.4
+  ngOnInit() {
+    this.transferencias = this.service.todas();
+  }
 }
+
--- ComecandoComOFramework/bytebank/src/app/services/transferencia.service.ts
+++ ComecandoComOFramework/bytebank/src/app/services/transferencia.service.ts
@@ -1,17 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Transferencia } from '../models/transferencia.models';
 //injectable pode ser invocado no construtor da classe appComponent
 @Injectable({
-  //declarado dessa maneira, enquanto a aplicação estiver ativa, esse service poderá ser invocado de qualquer módulo, ao contrario de por exemplo "provideIn: AppModule" que só poderia ser utilizado dentro desse módulo
+
   providedIn: 'root'
+  //declarado dessa maneira, enquanto a aplicação estiver ativa, esse service poderá ser invocado de qualquer módulo, ao contrario de por exemplo "provideIn: AppModule" que só poderia ser utilizado dentro desse módulo
 })
 export class TransferenciaService {
   private listaTransferencia: any[];
 
-constructor() {
+//
+// constructor() {
+//   this.listaTransferencia = [];
+// }
+
+//aula 6 - consumindo API Rest
+
+private url = 'http://localhost:3000/transferencias';
+
+constructor(private httpClient: HttpClient) {
   this.listaTransferencia = [];
 }
   get transferencias() {
     return this.listaTransferencia;
+  }
+
+  todas(): Observable<Transferencia[]>{
+    return this.httpClient.get<Transferencia[]>(this.url);
   }
 
   adicionar(transferencia: any) {
